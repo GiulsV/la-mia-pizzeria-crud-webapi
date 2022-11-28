e543b8dd8d721935d29869cb784c6e275afadda3
GiulsV <valente.giulia93@gmail.com>
2022-11-28 14:16:02 +0100

    Creato PizzaController con metodo Get()

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,34 @@
+﻿using la_mia_pizzeria_model.Models;
+using la_mia_pizzeria_model.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace la_mia_pizzeria_model.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        IPizzaRepository _pizzaRepository;
+        public PizzaController(IPizzaRepository pizzaRepository)
+        {
+            _pizzaRepository = pizzaRepository;
+        }
+
+        public PizzaController()
+        {
+
+        }
+
+        public IActionResult Test() 
+        {
+            return Ok("test");
+        }
+
+        public IActionResult Get() 
+        {
+            List<Pizza> pizzas = _pizzaRepository.All();
+            return Ok(pizzas);
+        }
+    }
+}
